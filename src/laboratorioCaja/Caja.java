4dc47696bd10aa3dfbc541d6b1151073c1f41867
DarioPro <DarioPro@LAPTOP-MLLB2SOQ>
2021-08-22 23:27:21 -0500

    creacion de proyecto caja

---
+++ src/laboratorioCaja/Caja.java
@@ -0,0 +1,22 @@
+package laboratorioCaja;
+
+public class Caja {
+	int ancho;
+	int profundo;
+	int alto;
+
+	public Caja() {
+    }
+
+    public Caja(int ancho, int alto, int profundo) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.profundo = profundo;
+    }
+
+    public int calcularVolumen() {
+        return ancho * alto * profundo;
+    }
+}
+
+
